b17e2da8c84a649582e31dc558381e43287b0093
Charlotte <charlotteleyser@icloud.com>
2023-09-27 13:44:21 +0200

    Add project files.

---
+++ TestProjekt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestProjekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
