864bfd342b7ecc9b2bbea66f7437a17a246ff940
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2023-09-27 13:54:43 +0200

    Lagt till en funktion som utforskar anvandarens gastronomiska preferenser och delar med sig av en djupgående analys av anvandarens svar

--- TestProjekt/Program.cs
+++ TestProjekt/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What's your favorite food? ");
+            string answer = Console.ReadLine();
+            answer = answer.ToLower();
+            if (answer == "lasagna")
+            {
+                Console.WriteLine("Yummy!");
+            }
+            else
+            {
+                Console.WriteLine("Disgusting!");
+            }
+            
         }
     }
 }
