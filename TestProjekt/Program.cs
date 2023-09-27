83f5befebf2b8d81bb1e19998c4fd991e0e7f68f
Huan <huanyang.ooi@gmail.com>
2023-09-27 13:55:15 +0200

    lagt till text samt namnförfrågan

--- TestProjekt/Program.cs
+++ TestProjekt/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hej World!");
+            Console.WriteLine("Vad heter du?");
+            string nameString = Console.ReadLine();
+            if (nameString = "Charlotte" || "Hjalmar" || "Huan") 
+            {
+                Console.WriteLine("Hej {0}", nameString);
+            }
+            Console.WriteLine("hmm")
         }
     }
 }
