efdc39b5106e427e31984671ced2f3ef420a49cf
Charlotte <charlotteleyser@icloud.com>
2023-09-27 13:52:19 +0200

    Skriv rätt siffra.

--- TestProjekt/Program.cs
+++ TestProjekt/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Skriv en siffra: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number != 5)
+            {
+                Console.WriteLine("Du har skrivit fel siffra.");
+            }
+            else
+            {
+                Console.WriteLine("Bra gjort.");
+            }
         }
     }
 }
